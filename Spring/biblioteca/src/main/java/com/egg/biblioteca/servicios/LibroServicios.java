7ef903b1adffd40a89d72f33be7e06425fb63ed7
Alfredo Nicolás Brade <alfredobrade@gmail.com>
2022-10-16 23:56:45 -0300

    a

--- Spring/biblioteca/src/main/java/com/egg/biblioteca/servicios/LibroServicios.java
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/servicios/LibroServicios.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+// servicios de la clase libro
 @Service
 public class LibroServicios {
     
