0d2f1a36675077948ee9bdb2a790d68735830e16
Alfredo N Brade <alfredobrade@gmail.com>
2022-10-27 22:09:51 -0300

    a

--- Spring/biblioteca/src/main/java/com/egg/biblioteca/servicios/AutorServicios.java
+++ Spring/biblioteca/src/main/java/com/egg/biblioteca/servicios/AutorServicios.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+//
 @Service
 public class AutorServicios {
     
