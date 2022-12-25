e6910df87882e1b543d0eb2842a2d3a85497c693
Alfredo Brade <alfredobrade@gmail.com>
2022-12-25 20:01:30 -0300

    a

---
+++ ReactJS/ejercicio2/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <Navbar/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
