14cf93338bcc10ed139a8a9329db5af47a346d40
Alfredo Brade <alfredobrade@gmail.com>
2023-02-06 10:08:59 -0300

    practica del domingo

--- JAVA_INTRO/JavaIntro6/src/JavaIntro6.java
+++ JAVA_INTRO/JavaIntro6/src/JavaIntro6.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class JavaIntro6 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+        JavaIntro6 funciones = new JavaIntro6();
         Scanner scanNumber = new Scanner(System.in);
         int num;
         do {
             System.out.println("ingrese un numero");
             num = scanNumber.nextInt();
-            if (num%2==0){
+            if (funciones.esPar(num)){
                 System.out.println("es par");
             }else {
                 System.out.println("es impar");
@@ -23,6 +23,11 @@ public class JavaIntro6 {
         }
         */
 
+
+    }
+
+    public boolean esPar(int a){
+        return (a%2==0);
 
     }
 }
